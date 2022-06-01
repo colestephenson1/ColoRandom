dc18a377ae98101792f1a6fb016654c7d0fcdece
Andrew Miller <andrew.r.miller45@gmail.com>
2022-06-01 10:58:11 -0700

    add class constructors, update readme

---
+++ Color.js
@@ -0,0 +1,17 @@
+class Color {
+    constructor(){
+        this.hexCode
+        this.locked = false
+    }
+
+    toggleLock(){
+        if (!this.locked) {
+            this.locked = true
+        } else
+            this.locked = false
+    }
+
+    randomHex(){
+        
+    }
+}
