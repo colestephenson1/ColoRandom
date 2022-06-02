841e062a57caede80fb96d0fc78763bd6744ee98
Andrew Miller <andrew.r.miller45@gmail.com>
2022-06-02 13:55:39 -0700

    refactor Color and Palette to remove params

--- Color.js
+++ Color.js
@@ -1,12 +1,26 @@
 class Color {
-    constructor(randomNum){
-        this.hexCode = randomNum;
+    constructor(){
+        this.hexCode = this.randomHex()
         this.locked = false;
-    };
+    }
+
     toggleLock(){
         if (!this.locked) {
             this.locked = true
         } else
             this.locked = false
-    };
-};
+    }
+
+    randomNum(array) {        
+        return Math.floor(Math.random() * array.length)     
+    } 
+
+    randomHex(){
+        var hex = `#`
+        var hexes = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+        for (let i = 0; i < 6; i++) {
+            hex += hexes[this.randomNum(hexes)]  
+        }    
+        return hex
+    }
+}
