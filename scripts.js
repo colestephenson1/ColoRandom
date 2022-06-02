75b1a7c13791a15da722841c1dcd7763c3edab21
Cole Stephenson <colestephenson94@gmail.com>
2022-06-01 18:56:57 -0600

    Achieve class to class random palette functionality

--- scripts.js
+++ scripts.js
@@ -4,42 +4,25 @@ var hexCodes = document.querySelectorAll('.hex-code');
 var newPaletteButton = document.querySelector('.new-palette-button');
 var savePaletteButton = document.querySelector('.save-palette-button');
 var lockAndUnlockIcon = document.querySelector('#icon1');
-// var box1 = document.querySelector('#box1');
-// var box2 = document.querySelector('#box2');
-
-
-window.addEventListener('load', randomHex);
-newPaletteButton.addEventListener('click', makeNewPalette);
-lockAndUnlockIcon.addEventListener('click', switchIcon)
-
-
-
-
-
-//Functions
-function randomHex() {
-  for (var i = 0; i < colorBoxes.length; i++) {
-    var randomNum =  Math.floor(Math.random()*16777215).toString(16);
-    colorBoxes[i].style.background = `#${randomNum}`;
-    hexCodes[i].innerText = `#${randomNum}`;
-  };
-};
-
-function makeNewPalette() {
-  randomHex();
-};
-
-function switchIcon() {
-  if (color.locked) {
-  lockAndUnlockIcon.src ="./assets/lock-icon.svg";
-} else {
-  lockAndUnlockIcon.src ="./assets/unlock-icon.svg"
-    };
-  };
 
 var palette = new Palette();
+
+
+window.addEventListener('load', displayPaletteTest);
+newPaletteButton.addEventListener('click', displayPaletteTest);
+
+function paletteTest() {
 palette.newColorInstance();
-console.log(palette);
-// function newColorInstance() {
-// var newColor = new Color(randomHex());
-// };
+palette.newColorInstance();
+palette.newColorInstance();
+palette.newColorInstance();
+palette.newColorInstance();
+};
+
+function displayPaletteTest(){
+  paletteTest();
+  for(var i = 0; i < palette.colors.length; i++) {
+    colorBoxes[i].style.background = palette.colors[i].hexCode;
+    hexCodes[i].innerText = palette.colors[i].hexCode;
+  } console.log(palette);
+};
