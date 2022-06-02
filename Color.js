ba0b392e46a961564f51197079fef86a726596b0
Cole Stephenson <colestephenson94@gmail.com>
2022-06-02 15:08:55 -0600

    Add semicolons

--- Color.js
+++ Color.js
@@ -2,25 +2,25 @@ class Color {
     constructor(){
         this.hexCode = this.randomHex()
         this.locked = false;
-    }
+    };
 
     toggleLock(){
         if (!this.locked) {
-            this.locked = true
+            this.locked = true;
         } else
-            this.locked = false
-    }
+            this.locked = false;
+    };
 
-    randomNum(array) {        
-        return Math.floor(Math.random() * array.length)     
-    } 
+    randomNum(array) {
+        return Math.floor(Math.random() * array.length);
+    };
 
     randomHex(){
-        var hex = `#`
-        var hexes = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+        var hex = `#`;
+        var hexes = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
         for (let i = 0; i < 6; i++) {
-            hex += hexes[this.randomNum(hexes)]  
-        }    
-        return hex
-    }
-}
+            hex += hexes[this.randomNum(hexes)];
+        }
+        return hex;
+    };
+};
--- Palette.js
+++ Palette.js
@@ -3,7 +3,6 @@ class Palette {
     this.id = Date.now();
     this.colors = [];
   };
-  
   newColorInstance(colorCount) {
     for (var i = 0; i < colorCount; i++) {
       var newColor = new Color();
